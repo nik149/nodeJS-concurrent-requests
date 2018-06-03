bbbd5c595f89474775b5edfb6435e940279a9023
Nikunj Sharma <nikunj.sharma92@gmail.com>
2018-06-03 22:18:13 +0530

    use requests module instead of axios

--- crawler.js
+++ crawler.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import cheerio from 'cheerio';
+import request from 'request';
 
 class Crawler {
 
@@ -11,89 +12,98 @@ class Crawler {
 
   updateLiveConnections(i) {
     this.liveConnections += i;
-    console.log("Num Connections: ", liveConnections);
-  }
-
-  start(initURL) {
-    this.updateLiveConnections(1);
-
-    axios.get(initURL).then(response => {
-      if(response.statusCode === 200) {
-        this.updateLiveConnections(-1);
-        //Push new URLs
-        this.url_list.concat(this.fetchNewURLs(response.data));
-
-        //Start the concurrent connections
-        this.crawl();
-        this.crawl();
-        this.crawl();
-        this.crawl();
-        this.crawl();
-
-      } else {
-        throw new Error('Invalid Response Code from Server ', response.statusCode);
-      }
-    })
-    .catch(error => {
-      this.updateLiveConnections(-1);
-      console.log(error.message);
-      process.exit(1);
-    });
+    console.log("Num Connections: ", this.liveConnections);
   }
 
   fetchNewURLs(body) {
     let $ = cheerio.load(body);
 
-    let links = [];
+    let self = this;
     $("a[href^='/']").each(function() {
-      links.push("http://www.medium.com/" + $(this).attr('href'));
+      self.url_list.push("http://www.medium.com/" + $(this).attr('href'));
     });
 
     $("a[href^='http']").each(function() {
       if($(this).attr('href').indexOf("medium.com") > -1){
-        links.push($(this).attr('href'));
+        self.url_list.push($(this).attr('href'));
       }
     });
 
-    return links;
+    $("a[href^='https']").each(function() {
+      if($(this).attr('href').indexOf("medium.com") > -1){
+        self.url_list.push($(this).attr('href'));
+      }
+    });
+  }
+
+  start(initURL) {
+    console.log("Initializing...");
+    this.updateLiveConnections(1);
+
+    let self = this;
+    request(initURL, (error, response, body) => {
+      if(error) {
+        self.updateLiveConnections(-1);
+        console.log(error.message);
+        process.exit(1);
+      } else {
+        if(response.statusCode === 200) {
+          self.updateLiveConnections(-1);
+
+          //Fetch new URLs
+          self.fetchNewURLs(body);
+
+          //Start the concurrent connections
+          self.crawl();
+          self.crawl();
+          self.crawl();
+          self.crawl();
+          self.crawl();
+        } else {
+          self.updateLiveConnections(-1);
+          console.log(error.message);
+          process.exit(1);
+        }
+      }
+    });
+
   }
 
   crawl() {
-    console.log("Crawling");
     if(!this.url_list.length) {
       console.log("URL List Empty");
       return;
     }
 
-    //Breadth First Crawling.
+    //Breadth First Crawling (FIFO).
     let newURL = this.url_list.shift();
 
+    //Check for duplicates
     if(newURL in this.url_visited) {
-      console.log("Duplicate");
-      return;
+      return this.crawl();
     }
+
+    console.log("Crawling: ", newURL);
+
     this.updateLiveConnections(1);
+
+    //mark as visited
     this.url_visited[newURL] = true;
 
-    axios.get(newURL)
-    .then(response => {
-      if(response.statusCode === 200) {
-        this.updateLiveConnections(-1);
-
-        //Concat new links from this page.
-        this.url_list.concat(this.fetchNewURLs(response.data));
-
-        //Add new to the queue.
-        this.crawl();
-      } else {
-        throw new Error('Invalid Response Code from Server ', response.statusCode);
+    let self = this;
+    request(newURL, (error, response, body) => {
+      if(response && response.statusCode === 200) {
+        //Fecth new links.
+        self.fetchNewURLs(body);
       }
-    })
-    .catch(error => {
-      this.updateLiveConnections(-1);
-      console.log(error.message);
-      this.crawl();
+
+      self.updateLiveConnections(-1);
+      //Add new crawler to the queue.
+      self.crawl();
     });
+
   }
 
 }
+
+export default Crawler;
