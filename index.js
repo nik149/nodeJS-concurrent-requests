7ba722ff7bf83e7e2da4ae5ff36a3c7ab27497ea
Nikunj Sharma <nikunj.sharma92@gmail.com>
2018-06-04 00:58:52 +0530

    flush terminal screen on start

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ if(process.argv[2] === 'custom') {
 } else if(process.argv[2] === 'async'){
   crawler = new CrawlerAsync(5);
 } else {
+  console.log("");
   console.log("Please provide valid arguments...");
   console.log("`npm start async` to launch crawler based on Async Library");
   console.log("`npm start custom` to launch custom crawler");
+  process.exit(0);
 }
 
+process.stdout.clearLine();
+process.stdout.write("\n\n");
+process.stdout.write("\x1B[2J");
+process.stdout.cursorTo(0,0);
 crawler.start('http://www.medium.com');
